5d3debefd41369cd685ff722432a98067dde2b3c
Rama <49932914+Ramakrishna1993@users.noreply.github.com>
2019-04-24 10:29:13 +0530

    changes

--- ramrepo/src/main/java/nit/ramrepo/App.java
+++ ramrepo/src/main/java/nit/ramrepo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "hi friends" );
         System.out.println( "Hello World! no worry" );
     }
 }
